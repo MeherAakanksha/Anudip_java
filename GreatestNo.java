42ef460b6a23269acbba612978877d13c0ed088e
MeherAakanksha <113004964+MeherAakanksha@users.noreply.github.com>
2022-09-08 01:21:03 -0700

    Add files via upload

---
+++ GreatestNo.java
@@ -0,0 +1,22 @@
+
+public class GreatestNo {
+
+	public static void main(String[] args) {
+		int a=9;
+		int b=5;
+		int c=7;
+		if(a>b)
+		{
+			System.out.println("a is greater " +a);
+		}
+		else if(b>c)
+		{
+			System.out.println("b is greater " +b);
+		}
+		else if(c>a)
+		{
+			System.out.println("c is greater " +c);
+		}
+	}
+
+}
