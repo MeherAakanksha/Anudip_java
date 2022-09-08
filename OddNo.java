c1a289ceef59e575471b0b6e60fa692713393184
MeherAakanksha <113004964+MeherAakanksha@users.noreply.github.com>
2022-09-08 01:21:37 -0700

    Add files via upload

---
+++ OddNo.java
@@ -0,0 +1,19 @@
+
+public class OddNo {
+
+	public static void main(String[] args) {
+		int i;
+		System.out.println("list of odd no from 1 to 20");
+	    for(i=1;i<=20;i++)
+		{
+	    	if(i%2!=0)
+	    	{  
+		
+			  System.out.println(  i+ " is odd no");
+	    	}
+			
+		}
+			
+
+	}
+}
